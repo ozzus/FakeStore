16c46e2ede662a9140897da36f972701fce2f196
Ozzus <serj.reintov228@gmail.com>
2025-05-03 09:56:13 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProductsPage from './pages/ProductsPage';
-import ProductPage from './pages/ProductPage';
-import NotFoundPage from './pages/NotFoundPage';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import ProductsPage from './pages/ProductsPage'
+import ProductPage from './pages/ProductPage'
+import NotFoundPage from './pages/NotFoundPage'
+import './App.css'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/product/:id" element={<ProductPage />} />
-        <Route path="/" element={<ProductsPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </Router>
-  );
+    <Routes>
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/product/:id" element={<ProductPage />} />
+      <Route path="/" element={<ProductsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
