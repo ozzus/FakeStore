e8960bc0186f2252e9a92bc9f1d4e204b10bbb93
Ozzus <serj.reintov228@gmail.com>
2025-05-03 09:24:04 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FakeStore/' // важно для корректного деплоя на GitHub Pages
+  base: '/FakeStore/' 
 })
